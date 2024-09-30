dc50edb7f73f250f9fce9a326ccd0a7ca545ec30
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-09-30 23:02:09 +0700

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,66 @@
+const { toBoolean } = require("validator");
+
+require("dotenv").config({});
+
+module.exports = {
+  local: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    dialectOptions: {
+      ssl: {
+        require: toBoolean(process.env.DB_SSL),
+        rejectUnauthorized: false,
+      },
+      bigNumberStrings: true,
+    },
+  },
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    dialectOptions: {
+      ssl: {
+        require: toBoolean(process.env.DB_SSL),
+        rejectUnauthorized: false,
+      },
+      bigNumberStrings: true,
+    },
+  },
+  staging: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    dialectOptions: {
+      ssl: {
+        require: toBoolean(process.env.DB_SSL),
+        rejectUnauthorized: false,
+      },
+      bigNumberStrings: true,
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CONNECTION,
+    dialectOptions: {
+      ssl: {
+        require: toBoolean(process.env.DB_SSL),
+        rejectUnauthorized: false,
+      },
+      bigNumberStrings: true,
+    },
+  },
+};
