fa63d554ddd45f0a0929658cc9b39712d3eeef27
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-10-01 14:02:44 +0700

    Fix routes

--- src/routes.js
+++ src/routes.js
@@ -9,3 +9,5 @@ router.use("/master/category", require("./modules/master/category"));
 router.use("/master/item-group", require("./modules/master/item_group"));
 router.use("/master/country", require("./modules/master/country"));
 router.use("/master/items", require("./modules/master/items"));
+
+module.exports = router;
