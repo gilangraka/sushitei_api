245fc0c282f12d95436eb1186066f3b05dd7cf2c
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-10-01 13:54:58 +0700

    Edit package.json

--- src/routes.js
+++ src/routes.js
@@ -8,3 +8,4 @@ router.use(express.urlencoded({ extended: true }));
 router.use("/master/category", require("./modules/master/category"));
 router.use("/master/item-group", require("./modules/master/item_group"));
 router.use("/master/country", require("./modules/master/country"));
+router.use("/master/items", require("./modules/master/items"));
